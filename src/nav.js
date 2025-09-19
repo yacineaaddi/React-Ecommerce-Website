5b56d2272f15699e7102f4a634cb66ee5a884661
yacine <yacineaaddi@gmail.com>
2025-09-19 23:17:07 +0100

    Add responsive styling to main header

--- src/nav.js
+++ src/nav.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
 import { CiHeart } from "react-icons/ci";
 import { FaShoppingCart } from "react-icons/fa";
 import { FaRegUser } from "react-icons/fa";
+import { FaBars } from "react-icons/fa6";
+import { IoClose } from "react-icons/io5";
 
 const Nav = ({ Auth, setAuth, userDetail }) => {
+  const [OpenNav, SetOpenNav] = useState(false);
+
   const Logout = () => {
     setAuth(false);
+  };
+  const navopen = () => {
+    SetOpenNav(true);
+  };
+  const navclose = () => {
+    SetOpenNav(false);
   };
   return (
     <>
@@ -37,6 +47,21 @@ const Nav = ({ Auth, setAuth, userDetail }) => {
           {/*Buttom navbar*/}
           <div className="mid_bar">
             <div className="content">
+              <div className="navicon">
+                {OpenNav ? (
+                  <>
+                    <div className="closenav" onClick={navclose}>
+                      <IoClose />
+                    </div>
+                  </>
+                ) : (
+                  <>
+                    <div className="navopen" onClick={navopen}>
+                      <FaBars />
+                    </div>
+                  </>
+                )}
+              </div>
               <div className="logo">
                 <img src="../public/img/logo.svg" alt="Logo"></img>
               </div>
@@ -56,28 +81,39 @@ const Nav = ({ Auth, setAuth, userDetail }) => {
               </div>
             </div>
           </div>
-          <div className="user-detail">
-            <div className="icon">
-              <FaRegUser />
-            </div>
-            <div className="detail">
-              {Auth ? (
-                <>
-                  <h2>{userDetail.Name}</h2>
-                  <p>{userDetail.Email}</p>
-                </>
-              ) : (
-                <h2>Please, Sign in</h2>
-              )}
+          <div className={`bottom_bar ${OpenNav ? "active" : ""}`}>
+            <div className="user-detail">
+              <div className="icon">
+                <FaRegUser />
+              </div>
+              <div className="detail">
+                {Auth ? (
+                  <>
+                    <h2>{userDetail.Name}</h2>
+                    <p>{userDetail.Email}</p>
+                  </>
+                ) : (
+                  <h2>Please, Sign in</h2>
+                )}
+              </div>
             </div>
             <ul>
               <li>
                 <Link to="/">Home</Link>
+              </li>
+              <li>
                 <Link to="/shop">Shop</Link>
+              </li>
+              <li>
                 <Link to="/about">About</Link>
+              </li>
+              <li>
                 <Link to="/contact">Contact</Link>
               </li>
             </ul>
+            <div className="offer">
+              <h2>30% off in winter sale</h2>
+            </div>
           </div>
         </div>
       </div>
