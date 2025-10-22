53daf8beaaf0ee132757859fc30e9584f67fe225
yacine <yacineaaddi@gmail.com>
2025-10-22 23:32:55 +0100

    Add Firestore add-to-cart function with quantity update

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ import { CiHeart, CiSearch } from "react-icons/ci";
 import { TfiReload } from "react-icons/tfi";
 import Footer from "./components/footer";
 import StarRating from "./components/starRating";
+import { db } from "./firebase"; /**/
+import {
+  doc,
+  collection,
+  addDoc,
+  getDocs,
+  updateDoc,
+} from "firebase/firestore"; /**/
+
 import "./App.css";
 
 const App = () => {
@@ -16,6 +25,64 @@ const App = () => {
   const [Auth, setAuth] = useState(false);
   const [products, setProducts] = useState(Product);
   const [search, setSearch] = useState("");
+
+  // Start : Firestore Add-to-Cart Function
+  //Creates a React state variable
+  const [cart, setCart] = useState([]); /**/
+  //Reference to the "cartData" collection in Firestore
+  const cartdbref = collection(db, "cartData"); /**/
+  // Function to handle adding a product to the cart
+  const addtocart = async (data) => {
+    //Check if user is logged in
+    if (Auth === false) {
+      // stop the function if not logged in
+      alert("Please Log In");
+      return;
+    } else {
+      //1-Fetch all existing cart documents from Firestore
+      const fetchcartdata = await getDocs(cartdbref);
+      //Convert Firestore documents into plain JavaScript objects
+      const cartsnap = fetchcartdata.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      //Check if this product already exists in the user’s cart
+      const findcartdata = cartsnap.find((x) => {
+        return userDetail.id === x.userId && data.id === x.CartId;
+      });
+
+      //2-If product already exists in the cart, just update its quantity
+      if (findcartdata) {
+        //Finds a reference to the exact document in Firestore
+        const cartdataref = doc(cartdbref, findcartdata.id);
+        //Updates that document’s Qty field by adding 1
+        await updateDoc(cartdataref, { Qty: findcartdata.Qty + 1 });
+        alert("Quantity Added Successfully");
+      }
+      //If product doesn’t exist, add it as a new document
+      else {
+        //3
+        const addCartData = await addDoc(cartdbref, {
+          userId: userDetail.id,
+          CartId: data.id,
+          Title: data.Title,
+          Cat: data.Cat,
+          Price: data.Price,
+          Img: data.Img,
+          SubCat: data.SubCat,
+          Brand: data.Brand,
+          Model: data.Model,
+          WebCode: data.WebCode,
+          Rating: data.Rating,
+          NumRev: data.NumRev,
+          State: data.State,
+          Qty: 1,
+        });
+        alert("Product Added To Cart");
+      }
+    }
+  };
+  // End : Firestore Add-to-Cart Function
 
   function OneProduct({ currEl }) {
     return (
@@ -37,7 +104,7 @@ const App = () => {
           </div>
           <h3>{currEl.Title}</h3>
           <h4>{currEl.Price} $</h4>
-          <button>Add To Cart</button>
+          <button onClick={() => addtocart(currEl)}>Add To Cart</button>
         </div>
       </div>
     );
@@ -78,7 +145,7 @@ const App = () => {
             </div>
           </div>
           <h4>{currEl.Price} $</h4>
-          <button>Add To Cart</button>
+          <button onClick={() => addtocart(currEl)}>Add To Cart</button>
         </div>
       </div>
     );
@@ -123,7 +190,7 @@ const App = () => {
               {currEl.DisountedPrice} $
             </h4>
           </div>
-          <button>Add To Cart</button>
+          <button onClick={() => addtocart(currEl)}>Add To Cart</button>
         </div>
       </div>
     );
