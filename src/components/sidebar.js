0db3d7944b61b4b78b27545f313fd8376f390be0
yacine <yacineaaddi@gmail.com>
2025-10-26 15:30:27 +0100

    Add functionality to add and remove items from Cart and Wishlist

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -43,93 +43,70 @@ const SideBar = ({
               <p>{currEl.Brand}</p>
 
               <div className="product-qty">
-                <button
-                  className="quantity"
-                  onClick={() => RemoveFromCart(currEl)}
-                >
-                  -
-                </button>
-                <input type="number" disabled value={currEl.Qty}></input>
-                <button className="quantity">+</button>
+                <button className="decrement">-</button>
+                <input
+                  type="number"
+                  className="value"
+                  disabled
+                  value={currEl.Qty}
+                ></input>
+                <button className="increment">+</button>
               </div>
             </div>
 
             <div className="delete">
-              <div className="deleteBtn">
+              <div
+                className="deleteBtn"
+                onClick={() =>
+                  sidebar === "wishlist"
+                    ? RemoveFromWishlist(currEl)
+                    : RemoveFromCart(currEl)
+                }
+              >
                 <MdDeleteForever />
               </div>
               <p>{`${currEl.Price * currEl.Qty} $`}</p>
             </div>
           </div>
-          {sidebar === "wishlist" ? (
+          {sidebar === "wishlist" && (
             <div className="add-to-cart" onClick={() => addtocart(currEl)}>
               Add To Cart
-            </div>
-          ) : (
-            <div className="add-to-cart" onClick={() => RemoveFromCart(currEl)}>
-              Remove From Cart
             </div>
           )}
         </div>
       </>
     );
-  } /*First Method*/
+  }
   const RemoveFromCart = async (data) => {
     try {
-      //1-Fetch all existing cart documents from Firestore
-      const fetchcartdata = await getDocs(cartdbref);
-      //Convert Firestore documents into plain JavaScript objects
-      const cartsnap = fetchcartdata.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      //Check if this product already exists in the user’s cart
-      const findcartdata = cartsnap.find((x) => {
-        return userDetail.id === x.userId && data.id === x.CartId;
-      });
+      await deleteDoc(doc(cartdbref, data.id));
 
-      //2-If product already exists in the cart, just update its quantity
-      if (findcartdata) {
-        //Finds a reference to the exact document in Firestore
-        const cartdataref = doc(cartdbref, findcartdata.id);
-        //Updates that document’s Qty field by adding 1
-        await deleteDoc(cartdataref);
+      const updatedCartData = await getDocs(cartdbref);
+      const updatedCart = updatedCartData.docs
+        .map((doc) => ({ id: doc.id, ...doc.data() }))
+        .filter((x) => x.userId === userDetail.id);
 
-        //1-Fetch all existing cart documents from Firestore
-        const fetchcartdata = await getDocs(cartdbref);
-        //Convert Firestore documents into plain JavaScript objects
-        const cartsnap = fetchcartdata.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        }));
-        setCart(cartsnap);
-        console.log(cartsnap);
-
-        alert("Product removed successfully from your cart!");
-      }
+      setCart(updatedCart);
+      alert("Product removed successfully from your cart!");
     } catch (error) {
       console.error("Error deleting product: ", error);
     }
-  }; /**/
+  };
+  const RemoveFromWishlist = async (data) => {
+    try {
+      await deleteDoc(doc(wishListdbref, data.id));
 
-  /*Second Method*/ /*
-        const RemoveFromCart = async (data) => {
-        
-          try {
-            await deleteDoc(doc(db, "cartData", data.id));
-          //1-Fetch all existing cart documents from Firestore
-                  const fetchcartdata = await getDocs(cartdbref);
-                  //Convert Firestore documents into plain JavaScript objects
-                  const cartsnap = fetchcartdata.docs.map((doc) => ({
-                    id: doc.id,
-                    ...doc.data(),
-                  }));
-            setCart(cartsnap);
-            console.log(cartsnap)
-             alert("Item deleted!");
-        }
-        catch (error) { console.error("Error deleting product: ", error) }
-        /**/
+      const updatedWishlistData = await getDocs(wishListdbref);
+      const updatedWishlist = updatedWishlistData.docs
+        .map((doc) => ({ id: doc.id, ...doc.data() }))
+        .filter((x) => x.userId === userDetail.id);
+
+      setWishlist(updatedWishlist);
+      alert("Product removed successfully from your wishlist!");
+    } catch (error) {
+      console.error("Error deleting product: ", error);
+    }
+  };
 
   useEffect(() => {
     fetchcartdata();
@@ -169,7 +146,6 @@ const SideBar = ({
     console.log(findwishListdata);
   };
 
-  /*const [quantity, setQuantity] = useState(1);*/
   return (
     <div className={`sidebar ${!sidebar ? "hidden" : ""}`}>
       <button className="close-btn" onClick={() => setSidebar((e) => "")}>
